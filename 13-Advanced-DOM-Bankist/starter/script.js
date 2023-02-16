f29b8cc78dd7376df1c9f21917e47df466a1bc3f
Oren Nudelman <orennu@gmail.com>
2023-02-16 22:49:00 +0200

    finish ch186

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -29,3 +30,54 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//////// selecting creating and deleting elements ////////
+
+// selecting elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const sections = document.querySelectorAll('.section');
+
+console.log(header);
+console.log(sections);
+
+console.log(document.getElementById('section--1'));
+
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// creating and inserting elements
+
+/// insertAdjacentHTML
+header.insertAdjacentHTML(
+  'afterbegin',
+  '<div style="font-size: 20px;">oren div</div>'
+);
+
+/// createElement
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent =
+//   'we use cookies for improved functionality and analytics.';
+message.innerHTML =
+  'we use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+header.prepend(message);
+// setTimeout(() => header.append(message), 3000);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+// delete
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+    // message.parentElement.removeChild(message);
+  });
