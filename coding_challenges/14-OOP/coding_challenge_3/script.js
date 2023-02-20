9e4eb0334580cdb68b5f776d1e5c8f48c2568403
Oren Nudelman <orenn@checkpoint.com>
2023-02-20 17:18:24 +0200

    finish challenge #3

--- coding_challenges/14-OOP/coding_challenge_3/script.js
+++ coding_challenges/14-OOP/coding_challenge_3/script.js
@@ -9,3 +9,51 @@ Test data:
 § Data car 1: 'Tesla' going at 120 km/h, with a charge of 23%
 GOOD LUCK 😀
 */
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} speed is ${this.speed}km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} speed is ${this.speed}km/h`);
+};
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`
+  );
+};
+
+const tesla = new EV('Tesla', 120, 23);
+
+console.log(tesla);
+console.log(tesla.charge);
+tesla.chargeBattery(90);
+console.log(tesla.charge);
+
+tesla.accelerate();
+tesla.accelerate();
+tesla.accelerate();
+tesla.accelerate();
+tesla.brake();
+tesla.brake();
