4d9513614163fec6d55db85e871ac10de47ff537
Oren Nudelman <orenn@checkpoint.com>
2023-03-02 21:10:54 +0200

    finish ep254

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -224,5 +224,71 @@ const getCountryData = function (country) {
     .then(data => renderCountry(data, 'neighbour'));
 };
 
-getCountryData('jordan');
+getCountryData('brazil');
 */
+
+//////// handling rejected promises ////////
+
+const renderCountry = function (data, className = '') {
+  const html = `
+              <article class="country ${className}">
+                  <img class="country__img" src="${data.flag}" />
+                  <div class="country__data">
+                      <h3 class="country__name">${data.name}</h3>
+                      <h4 class="country__region">${data.region}</h4>
+                      <p class="country__row"><span>👫</span>${(
+                        +data.population / 1000000
+                      ).toFixed(1)}M people</p>
+                      <p class="country__row"><span>🗣️</span>${
+                        data.languages[0].name
+                      }</p>
+                      <p class="country__row"><span>💰</span>${
+                        data.currencies[0].name
+                      }</p>
+                  </div>
+              </article>
+              `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+};
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+
+      if (!neighbour) return;
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data, 'neighbour');
+      const neighbour = data.borders?.[0];
+
+      if (!neighbour) return;
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    // then is called when promise is fullfilled
+    .then(data => renderCountry(data, 'neighbour'))
+    // catch is called when promise is rejected
+    .catch(err => {
+      console.error(`${err} ☣☣☣`);
+      renderError(`something went wrong ☣☣☣ ${err.message}. Try again!`);
+    })
+    // finally is always called
+    .finally(() => {
+      console.log('I always happen yey');
+      countriesContainer.style.opacity = 1;
+    });
+};
+
+btn.addEventListener('click', function () {
+  getCountryData('addad');
+});
